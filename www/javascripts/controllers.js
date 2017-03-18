20e8e47f5e429ff1a177436ee7329cc9a847ffc8
ayinbunga <ayinrain94@gmail.com>
2017-03-18 15:37:25 +0800

    first upload

---
+++ www/javascripts/controllers.js
@@ -0,0 +1,30 @@
+
+//side menu controller
+
+window.fn = {};
+
+    window.fn.open = function() {
+      var menu = document.getElementById('menu');
+      menu.open();
+    };
+
+    window.fn.load = function(page) {
+      var content = document.getElementById('content');
+      var menu = document.getElementById('menu');
+      content.load(page)
+      .then(menu.close.bind(menu));
+    };
+
+//login function
+
+var login = function() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  if (username === 'bob' && password === 'secret') {
+    ons.notification.alert('Congratulations!');
+  }
+  else {
+    ons.notification.alert('Incorrect username or password.');
+  }
+};
---
+++ www/javascripts/db.js
@@ -0,0 +1,40 @@
+
+// initiate fitmama websql database
+ 
+ var db = null;
+ var db = openDatabase('fitmama', '1', 'fitmama', 2 * 1024 * 1024);
+	db.transaction(function(tx){
+		//tx.executeSql('DROP TABLE IF EXISTS user');
+		//tx.executeSql('DROP TABLE IF EXISTS userimage');
+		//tx.executeSql('DROP TABLE IF EXISTS recipes');
+		//tx.executeSql('DROP TABLE IF EXISTS tips');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS user(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, created unique, email, username UNIQUE, password)');
+		//tx.executeSql('CREATE TABLE IF NOT EXISTS recipes(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, username, title, recipe, recipe_img, timestamp)');
+		//tx.executeSql('CREATE TABLE IF NOT EXISTS tips(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, username, title, content, timestamp, category)');
+	});
+
+$('document').ready(function(){
+		var db = openDatabase('fitmama', '1', 'fitmama', 2 * 1024 * 1024);
+		
+	db.transaction(function(tx){
+			tx.executeSql('SELECT * FROM user', [], querySuccess, errorCB);	
+	});
+	
+	function querySuccess(tx, results){
+			var len = results.rows.length;
+				if(len > 0){
+					
+					console.log("Currently have "+len+" users registered");
+					}
+				
+				else{
+					console.log("No users registered yet");
+					localStorage.setItem("username", "");
+				}
+				
+		}
+		function errorCB(err){
+		 alert("Error" + err.code);
+		}
+	});
+
